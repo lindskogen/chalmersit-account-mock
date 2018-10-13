95bbfc5caf1c7b0a7a00c8b40c5bb5c349b74c6e
Johan Lindskogen <johan.lindskogen@gmail.com>
2018-10-13 21:24:46 +0200

    Add oauth callbacks and redirects

--- index.js
+++ index.js
@@ -2,23 +2,62 @@ const adminmock = require('./mocks/adminmock.json');
 const fkitmock = require('./mocks/fkitmock.json');
 const defaultmock = require('./mocks/defaultmock.json');
 
+const { send } = require('micro');
+
+const mocks = {
+	fkitsmurf: fkitmock,
+	adminsmurf: adminmock,
+	smurf: defaultmock
+};
+
+const tokenResponse = {
+	access_token: 'i am access token'
+};
 
 const getSelfMock = () => {
-	const mock = process.env.MOCK;
+	const id = process.env.MOCK + 'smurf';
+	return mocks[id] || defaultmock
+}
 
-	if (mock === 'admin')  {
-		return adminmock;
-	} else if (mock === 'fkit') {
-		return fkitmock;
+const getSpecificMock = (pathname) => {
+	const [_, _users, id] = pathname.replace('.json', '').split('/');
+
+	if (id === undefined) {
+		return Object.values(mocks);
 	} else {
-		return defaultmock;
+		return mocks[id] || defaultmock;
 	}
 }
 
-module.exports = (req) => {
-	if (req.url === '/me.json') {
+const redirect = (res, url) => {
+	res.setHeader('Location', url);
+	return send(res, 302, null);
+}
+
+module.exports = (req, res) => {
+	const url = new URL(req.url, 'http://localhost');
+
+	console.log(req.url);
+
+	if (url.pathname === '/oauth/authorize') {
+		const params = url.searchParams;
+		const [redirect_uri, state] = [params.get('redirect_uri'), params.get('state')];
+		const location = redirect_uri + '?state=' + state;
+
+		return redirect(res, location);
+	} else if (url.pathname === '/oauth/token') {
+		return tokenResponse;
+	} else if (url.pathname === '/logout') {
+		const params = url.searchParams;
+		const return_to = params.get('return_to');
+
+		return redirect(res, return_to);
+	} else if (url.pathname === '/me.json') {
 		return getSelfMock();
+	} else if (url.pathname.indexOf('/users') !== -1) {
+		return getSpecificMock(url.pathname);
 	} else {
+		console.log('fallthrough:', req.url);
 		return defaultmock;
 	}
 }
